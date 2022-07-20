4aea3deb71cba8884f80fddcdd406cd7902f4238
danielcm585 <danielcm585@gmail.com>
2022-07-20 10:25:58 +0700

    fix get post routes

--- controllers/postController.js
+++ controllers/postController.js
@@ -26,13 +26,14 @@ module.exports.createMain = async (req, res, next) => {
 
 module.exports.getAll = async (req, res, next) => {
   const { id } = req.params
-  const posts = await Post.find({ group: id })//.populate('creator', '-password')
-  res.status(200).json(posts)
+  const group = await Group.findById(id)
+  res.status(200).json({ posts: group.posts, pinned: group.pinned })
 }
 
 module.exports.getAllMain = async (req, res, next) => {
-  const posts = await Post.find({ group: process.env.MAIN_GROUP_ID })//.populate('creator', '-password')
-  res.status(200).json(posts)
+  const group = await Group.findById(process.env.MAIN_GROUP_ID)
+  console.log(group)
+  res.status(200).json({ posts: group.posts, pinned: group.pinned })
 }
 
 module.exports.getOne = async (req, res, next) => {
