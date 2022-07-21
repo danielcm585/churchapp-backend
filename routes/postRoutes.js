8912a21c7a4031515b64ec0152149a866ff2f3e6
danielcm585 <danielcm585@gmail.com>
2022-07-21 22:04:39 +0700

    change method

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -8,11 +8,11 @@ const router = express.Router()
 
 router.post('/', isLoggedIn, isAdmin, catchAsync(post.createMain))
 router.post('/:id', isLoggedIn, isGroupMember, catchAsync(post.create))
-router.post('/pin/:id', isLoggedIn, isGroupMember, catchAsync(post.pin))
 router.get('/all', catchAsync(post.getAllMain))
 router.get('/all/:id', isLoggedIn, isGroupMember, catchAsync(post.getAll))
 router.get('/one/:id', catchAsync(post.getOne))
 router.put('/:id', isLoggedIn, isPostCreator, catchAsync(post.edit))
+router.put('/pin/:id', isLoggedIn, catchAsync(post.pin))
 router.delete('/:id', isLoggedIn, isPostCreator, catchAsync(post.delete))
 
 module.exports = router
