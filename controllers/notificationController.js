03802a569ffd9f3ddcad5b55ddf9d83c60be02c0
danielcm585 <danielcm585@gmail.com>
2022-07-26 17:29:44 +0700

    fix read one

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -15,14 +15,14 @@ module.exports.getOne = async (req, res, next) => {
 
 module.exports.readAll = async (req, res, next) => {
   const user = await User.findById(req.user._id)
-  for (let notification of user.notifications) {
-    await Notification.findByIdAndUpdate(notification, { read: true })
+  for (let notifId of user.notifications) {
+    await Notification.findByIdAndUpdate(notifId, { read: true })
   }
   res.status(200).json('All notification read')
 }
 
 module.exports.readOne = async (req, res, next) => {
-  const { id } = await User.findById(req.user._id)
+  const { id } = req.params
   await Notification.findByIdAndUpdate(id, { read: true })
   res.status(200).json('Notification read successfully')
 }
