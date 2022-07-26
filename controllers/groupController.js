1cf6625dd3f89f9af30a16329b10546911e06764
danielcm585 <danielcm585@gmail.com>
2022-07-26 16:03:35 +0700

    fix group getOne

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -28,11 +28,10 @@ module.exports.getMine = async (req, res, next) => {
 module.exports.getOne = async (req, res, next) => {
   const { id } = req.params
   const group = await Group.findById(id)
-  group.populate('members','-password')
-  group.populate('leaders','-password')
-  group.populate('pendings','-password')
-  group.populate('invites','-password')
-  // await group.populate('events') // TODO: Add feature later
+    .populate('members',['_id','name','photo'])
+    .populate('leaders',['_id','name','photo'])
+    .populate('pendings',['_id','name','photo'])
+    .populate('invites',['_id','name','photo'])
   res.status(200).json(group)
 }
 
